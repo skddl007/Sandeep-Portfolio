7c0e4b4490315b473d2b46ba3d8dc07e25b49ff4
Rajat Malviya <143414044+rajat-malvi@users.noreply.github.com>
2024-10-23 15:34:22 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  let count = 0
   expect(linkElement).toBeInTheDocument();
+  
 });
